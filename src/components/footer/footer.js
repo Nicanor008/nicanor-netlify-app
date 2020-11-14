ee907544d8cef4ba1fc5ebc27064bde7e44cf9bd
Nicanor008 <nicanorkorir008@gmail.com>
2020-11-14 12:56:29 +0300

    Email Link, compose new mail

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,11 +5,29 @@ import Gmail from "../../images/projects/mail.svg";
 function Footer() {
   return (
     <div className="footer">
-      <span className="footer-text">Talk to me Let’s Chat</span>
-      <img src={HrLine} alt="horizontal line" className="footer-hrLine" height="5" width="85%" />
+      <span className="footer-text">Talk to me, Let’s Chat</span>
+      <img
+        src={HrLine}
+        alt="horizontal line"
+        className="footer-hrLine"
+        height="5"
+        width="85%"
+      />
       <div className="footer-contacts">
         <center>
-          <img src={Gmail} alt="email" className="footer-contacts-mail" height="" width="" />
+          <a
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=nicanorkorir008@gmail.com&su=Talk to me, Let's Chat&body=Message"
+            target="blank"
+            rel="nofollow noreferrer"
+          >
+            <img
+              src={Gmail}
+              alt="email"
+              className="footer-contacts-mail"
+              height=""
+              width=""
+            />
+          </a>
         </center>
       </div>
     </div>
