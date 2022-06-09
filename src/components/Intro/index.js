b8803990af8b4a587233891d71de0d9d7875a79a
Nicanor Korir <nicanor@Nicanors-MacBook-Pro.local>
2022-06-09 13:05:36 +0300

    Mailto fix on link

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -85,7 +85,7 @@ const Intro = () => {
             text="Twitter"
           />
           <SocialImageLink
-            to="#"
+            to="mailto:nicanorkorir008@gmail.com"
             image={
               <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
